19e9776072baa3d2ac41a3606fea417e3d7c3b71
icecong <icezhicong@gmail.com>
2019-07-12 14:26:18 +0800

    add react demo

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { NavLink, Route, Switch, Redirect, Link } from 'react-router-dom';
+//import {} from 'react-router-dom';
+import App from './App';
+
+
+ReactDOM.render(<App />, 
+
+    document.getElementById('root')
+    //返回id为root的一个元素
+    );
+
+
